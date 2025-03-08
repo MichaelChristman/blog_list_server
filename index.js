143b31eaa97affb984d7e2ec9e9774f62ebf2cf0
Michael Christman <christman.michaelj@gmail.com>
2025-03-08 14:02:23 -0600

    Working npm application, GET and POST requests interact with the DB

--- index.js
+++ index.js
@@ -1,37 +1,79 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
 
-let blogs = [
-    {
-      id: "1",
-      title: "My first blog",
-      author: "Michael Christman",
-      url: "youtube.com",
-      likes: 10
-    },
-    {
-      id: "2",
-      title: "Another blog",
-      author: "Michael Christman",
-      url: "youtube.com",
-      likes: 15
-    },
-    {
-        id: "3",
-        title: "Third blog",
-        author: "Michael Christman",
-        url: "youtube.com",
-        likes: 20
-    }
-  ]
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
 
-  app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-  })
-  
-  app.get('/api/blogs', (request, response) => {
-    response.json(blogs)
-  })
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = "mongodb+srv://christmanmichaelj:u6FUJkpTK4UjLnyH@cluster0.6lp0j.mongodb.net/blogList?retryWrites=true&w=majority&appName=Cluster0"
+//possibly remove 
+mongoose.set('strictQuery',false)
+
+mongoose.connect(mongoUrl)
+
+app.use(cors())
+app.use(express.json())
+
+// const blog = new Blog({
+//     id: "1",
+//     title: "My first blog",
+//     author: "Michael Christman",
+//     url: "youtube.com",
+//     likes: 10
+// })
+
+// let blogs = [
+//     {
+//       id: "1",
+//       title: "My first blog",
+//       author: "Michael Christman",
+//       url: "youtube.com",
+//       likes: 10
+//     },
+//     {
+//       id: "2",
+//       title: "Another blog",
+//       author: "Michael Christman",
+//       url: "youtube.com",
+//       likes: 15
+//     },
+//     {
+//         id: "3",
+//         title: "Third blog",
+//         author: "Michael Christman",
+//         url: "youtube.com",
+//         likes: 20
+//     }
+//   ]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Blog List Backend Connected</h1>')
+})
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
 
 const PORT = 3003
 app.listen(PORT, () => {
